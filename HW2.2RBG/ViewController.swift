e08f7f99d66ec7fe76b69a364454d93d1d27cbd1
Vadim Gamzaev <Vadosbox@gmail.com>
2019-09-23 14:35:57 +0300

    Initial Commit

---
+++ HW2.2RBG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.2RBG
+//
+//  Created by Вадим Гамзаев on 23/09/2019.
+//  Copyright © 2019 Вадим Гамзаев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
